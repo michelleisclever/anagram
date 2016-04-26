66403722816fd947bb88915193f428d69d32247a
Michelle McManus <michelleisclever@gmail.com>
2016-04-26 18:35:45 -0500

    updating records

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -21,6 +21,23 @@ end
     erb :"/words/new"
 end
 
+get '/words/:id/edit' do
+    @word = Word.find(params[:id])
+    erb :"/words/edit"
+end
+
+put '/words/:id' do
+    @word = Word.find(params[:id])
+    @word.text = params[:text]
+    if @word.valid?
+        @word.save
+        redirect "/words/#{@word.id}"
+    else
+        @errors = "Please be sure to add all input."
+    end
+    erb :"/words/edit"
+end
+
 get '/words/:id' do
     @word = Word.find(params[:id])    
     erb :"/words/show"
